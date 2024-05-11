80b50c0fde0564a34f8f3ff986e785609a95bc9f
faridhidayatulloh <syariahibtisama28@gmail.com>
2024-05-11 17:06:56 +0700

    transaksi

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -63,11 +63,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="<?= base_url('template') ?>/distt/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
-          <a href="#" class="d-block">Ahmar Ramzi</a>
+          <a href="#" class="d-block">Farid Hidayatulloh</a>
         </div>
       </div>
 
